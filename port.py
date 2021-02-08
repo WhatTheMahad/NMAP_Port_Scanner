923fed8389ac45a4da8562fd7f130eb6ce7553cd
WhatTheMahad <mahadnaveed22@gmail.com>
2021-02-08 05:38:02 -0500

    First Commit. Program takes IP as an Input andscans with NMAP, shows open ports with NMAP dictionary. First Version has TCP network protocol. UDP and rest scans will be updated soon.

---
+++ port.py
@@ -0,0 +1,24 @@
+#!/bin/bash/python3
+import nmap
+
+scanner = nmap.PortScanner()
+print ("NMAP Port_Scanner.\n By @WhatTheMahad")
+ip_address = input ("Enter IP ")
+print ("IP is: ", ip_address)
+type(ip_address)
+resp = input ("""\n Enter Fucntion to Perform
+				1. SYN ACK Scan
+				2. UDP Scan
+				3. Comprehensive Scan \n""")
+print("You selected option ", resp)
+
+
+if resp == '1':
+	print("NMAP Version: ", scanner.nmap_version())
+	scanner.scan(ip_address,'1-1024','-v -sS')
+	print(scanner.scaninfo())
+	print("IP Status: ", scanner[ip_address].state())
+	print(scanner[ip_address].all_protocols())
+	print("Open Ports: ",scanner[ip_address]['tcp'].keys())
+elif resp >= 20:
+	print ("Invalid Input")
