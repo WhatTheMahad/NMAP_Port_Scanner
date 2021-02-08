1cc2d604e8d45225c4be0ff22bfc177ea91656a4
WhatTheMahad <mahadnaveed22@gmail.com>
2021-02-08 11:22:40 -0500

    2nd Commit, added additional scans i.e., UDP and Comprehensive scans, that scans the target I.P thorougly.

--- port.py
+++ port.py
@@ -3,7 +3,7 @@ import nmap
 
 scanner = nmap.PortScanner()
 print ("NMAP Port_Scanner.\n By @WhatTheMahad")
-ip_address = input ("Enter IP ")
+ip_address = input ("Enter IP: ")
 print ("IP is: ", ip_address)
 type(ip_address)
 resp = input ("""\n Enter Fucntion to Perform
@@ -19,6 +19,25 @@ if resp == '1':
 	print(scanner.scaninfo())
 	print("IP Status: ", scanner[ip_address].state())
 	print(scanner[ip_address].all_protocols())
-	print("Open Ports: ",scanner[ip_address]['tcp'].keys())
+	print("Open Ports: " ,scanner[ip_address]['tcp'].keys())
+
+elif resp == '2':
+	print ("NMAP Version: ",scanner.nmap_version())
+	scanner.scan(ip_address,'1-1024','-v -sU')
+	print(scanner.scaninfo())
+	print("IP Status: ", scanner[ip_address].state())
+	print(scanner[ip_address].all_protocols())
+	print("Open Ports: " ,scanner[ip_address]['udp'].keys())
+
+elif resp == '3':
+	print ("NMAP Version: ",scanner.nmap_version())
+	scanner.scan(ip_address,'1-1024','-v -sS -sV -sC -A -O')
+	print(scanner.scaninfo())
+	print("IP Status: ", scanner[ip_address].state())
+	print(scanner[ip_address].all_protocols())
+	print("Open Ports: " ,scanner[ip_address]['tcp'].keys())
+
+
+
 elif resp >= 20:
-	print ("Invalid Input")
+	print("Invalid")
